d16a6eb95e8e3287f032996ab5e3e75b5946f78c
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2019-08-29 17:55:27 +0200

    done kruskal-wallis tests

--- analysis.R
+++ analysis.R
@@ -52,12 +52,12 @@ df <- df %>%
 #   filter(id %in% df$id)),
 #   "df_long_final.csv")
 
-kruskal.test(dist_osa ~ `Do eksperymentu trzeba przygotować aparaturę. Czy potrafisz włączyć tryb prywatny (incognito) w swojej przeglądarce internetowej?`,
-             data = df) 
-kruskal.test(dist_osa ~ `Rodzaj studiów`, data = df)
-kruskal.test(dist_osa ~ `Twoja płeć`, data = df)
-kruskal.test(dist_osa ~ `Rodzaj studiów`, data = df)
-kruskal.test(dist_osa ~ `Twój rok urodzenia`, data = df)
-# all not significant
+# kruskal.test(dist_osa ~ `Do eksperymentu trzeba przygotować aparaturę. Czy potrafisz włączyć tryb prywatny (incognito) w swojej przeglądarce internetowej?`,
+#              data = df) 
+# kruskal.test(dist_osa ~ `Rodzaj studiów`, data = df)
+# kruskal.test(dist_osa ~ `Twoja płeć`, data = df)
+# kruskal.test(dist_osa ~ `Rodzaj studiów`, data = df)
+# kruskal.test(dist_osa ~ `Twój rok urodzenia`, data = df)
+# # all not significant
 
-table(df$incognito, df$normal)
+
