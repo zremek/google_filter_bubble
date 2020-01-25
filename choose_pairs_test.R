cd4567ffc0c7f419cdcb4b355ade591ad89c41bc
Remigiusz <remigiuszzulicki@gmail.com>
2020-01-25 13:37:32 +0100

    check number of pairs created

--- choose_pairs_test.R
+++ choose_pairs_test.R
@@ -3,12 +3,20 @@ library(stringdist)
 
 df = read_csv("df_final.csv")
 
-df_normal <- t(combn(df$normal, 2))
-colnames(df_normal) <- c("i", "ii")
+# put all possibe pairs of search result sets into a tibble
+# to calculate osa distance between participants
+# in each browser mode
 
+df_normal <- t(combn(df$normal, 2))
+
+colnames(df_normal) <- c("i", "ii")
 df_normal <- as_tibble(df_normal)
 
 
+# check number of pairs created
+choose(73, 2) == length(df_normal$i)
+
+# calculate osa distance
 df_normal <- df_normal %>%
   mutate(dist_osa = stringdist::stringdist(a = i,
                                            b = ii,
@@ -16,7 +24,7 @@ df_normal <- df_normal %>%
 
 summary(df_normal$dist_osa)         
 
-
+# the same for incognito mode
 df_incognito <- t(combn(df$incognito, 2))
 colnames(df_incognito) <- c("i", "ii")
 
@@ -30,6 +38,7 @@ df_incognito <- df_incognito %>%
 
 summary(df_incognito$dist_osa)
 
+# quick plots
 qplot(x = dist_osa, data = df_normal, 
       geom = "bar", xlab = "osa: normal between users")
 
@@ -38,3 +47,4 @@ qplot(x = dist_osa, data = df_incognito,
 
 qplot(x = dist_osa, data = df, 
       geom = "bar", xlab = "private vs normal - same user")
+
